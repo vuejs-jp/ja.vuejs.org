14b85826c2504d3219b3f3adce4577c08ec0d738
Naoki Endoh <naoki.endoh@gmail.com>
2021-05-10 21:57:28 +0900

    SSR guide
    https://github.com/vuejs/docs-next/commit/799b2c871f8a6d8cbab101b511564f7e2d3e840c#diff-520c551813b4a6b395629e927acb684917c702b7be5d9c1d600b4ecb685fa6cc

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -211,16 +211,16 @@ const sidebar = {
       ]
     },
   ],
-  // ssr: [
-  //   ['/guide/ssr/introduction', 'Introduction'],
-  //   '/guide/ssr/getting-started',
+  ssr: [
+    ['/guide/ssr/introduction', 'Introduction'],
+    '/guide/ssr/getting-started',
   //   '/guide/ssr/universal',
   //   '/guide/ssr/structure',
   //   '/guide/ssr/build-config',
   //   '/guide/ssr/server',
   //   '/guide/ssr/routing',
   //   '/guide/ssr/hydration'
-  // ],
+  ],
   contributing: [
     {
       title: 'ドキュメントへの貢献',
